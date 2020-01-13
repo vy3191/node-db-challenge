ff3f7b99f9dbc349399ba2d61bb0c48879b1b2a5
vy3191 <venkyafhs@gmail.com>
2020-01-13 18:39:46 -0500

    MVP Completed

--- models/task-model.js
+++ models/task-model.js
@@ -1,7 +1,16 @@
 const db = require("../data/db-config");
 
 function findTasks() {
-   return db("tasks").select();
+   return db("tasks as t")
+            .join("projects as p", "p.id", "t.project_id")
+            .select("t.id as Task ID",                  
+                   "p.name as Project Name",             
+                   "p.description as Project Descrition",
+                    "t.project_id as Project ID", 
+                    "t.description as Task Description",
+                    "t.notes as Task Notes",
+                   "t.completed as Is Task Completed?" )
+            .orderBy("t.project_id")                     
 }
 
 async function addTasks(task) {
