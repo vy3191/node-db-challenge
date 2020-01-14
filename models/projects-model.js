f63dbdfe9e68c3c017774628f828af0fb60ed8d1
vy3191 <venkyafhs@gmail.com>
2020-01-13 19:48:14 -0500

    Completed Stretch goal

--- models/projects-model.js
+++ models/projects-model.js
@@ -10,11 +10,20 @@ async function addProjects(newProject) {
 }
 
 async function findAll(id) {
-   return  db("projects as p")
-           .join("tasks as t", "p.id", "t.project_id")
-           .where({"p.id":id})
-           .first()
-           .select()
+   const projects =  await db("projects").where({id}).first();
+   const tasks = await db("tasks as t")
+                         .where({"t.project_id": id})
+                         .select("t.id as Task Id",
+                                 "t.project_id as Project ID",
+                                 "t.description as Task Description", 
+                                 "t.notes as Task Notes",
+                                 "t.completed as Is Task Completed" );
+   const resources = await db("resources as r")
+                            .join("projects_resources as p_r", "p_r.resource_id", "r.id")
+                            .where({"p_r.project_id": id}) 
+                            .select("r.id as Resource ID", "r.name as Resouce Name", "r.description as Description");
+                            
+   return { ...projects, tasks, resources};                            
 }
 
 module.exports = {
