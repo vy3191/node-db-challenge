7bac9ba693d445bf444851e514e92d9cfafd9a27
vy3191 <venkyafhs@gmail.com>
2020-01-13 19:20:07 -0500

    Still working on stretch goal

--- models/projects-model.js
+++ models/projects-model.js
@@ -9,7 +9,16 @@ async function addProjects(newProject) {
    return db("projects").where({id}).first();
 }
 
+async function findAll(id) {
+   return  db("projects as p")
+           .join("tasks as t", "p.id", "t.project_id")
+           .where({"p.id":id})
+           .first()
+           .select()
+}
+
 module.exports = {
    findProjects,
-    addProjects
+    addProjects,
+    findAll
 }
--- routes/projects.js
+++ routes/projects.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = express.Router();
-const projectModle = require("../models/projects-model");
+const router = express.Router({mergeParams:true});
+const projectModel = require("../models/projects-model");
 
 router.get("/", async (req,res,next) => {
    try {
-      const projects = await projectModle.findProjects();
+      const projects = await projectModel.findProjects();
       const mappedProjects = projects.map( (project) => {
          if(project.completed === 1) project.completed = true;
          if(project.completed === 0) project.completed = false;
@@ -12,18 +12,29 @@ router.get("/", async (req,res,next) => {
       })
       res.status(200).json(mappedProjects);
    }catch(error) {
-     next(err);
+     next(error);
    }
+});
+
+router.get("/:id/tasks-resources", async (req,res,next) => {
+     try {
+         const {id} = req.params;
+         console.log(id)
+         const projects = await projectModel.findAll(id);
+         res.status(200).json(projects);
+     } catch(error) {
+        next(error)
+     }
 });
 
 router.post("/", async (req,res,next) => {
   try {
     const newProject = req.body;
-    const addedProject = await projectModle.addProjects(newProject);
+    const addedProject = await projectModel.addProjects(newProject);
     res.status(201).json(addedProject);
 
   }catch(error) {
-    next(err);
+    next(error);
   }
 });
 
